c09947ec8ecc5b7199595873b463386f5ffff71b
lopesh37z <henry.batista.lopes@escola.pr.gov.br>
2026-08-21 10:46:29 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded, ()=>{
+document.addEventListener("DOMContentLoaded, ()=> {
+    // Seleciona o botão de curtida e o ícone de coração
     const likeBtn = document.queryselector(".left-actions .action-btn:first-child");
-})
+    if (!likeBtn) return;
+
+    const likeSvg = likeBtn.querySelector("svg");
+     
+    // contador
+    let textNode = Array.from(likeBtn.childNodes).find(
+        (node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== ""
+    );
+
+    // Zerando o contador inicial
+    let count = 0;
+
+    // Atualiza 
+    if (textNode)  {
+        textNode.textContent = 0;
+    }
